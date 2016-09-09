f4f05e91008cc09d415f70c4824826f0a9767027
Lucas Pires <lucas.pires.mattos@gmail.com>
2016-09-08 21:17:17 -0300

    Added service provider

---
+++ src/LakerServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace FlyingLuscas\Laker;
+
+use Illuminate\Support\ServiceProvider;
+
+class LakerServiceProvider extends ServiceProvider
+{
+    /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register bindings in the container.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
